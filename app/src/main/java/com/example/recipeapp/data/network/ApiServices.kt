71f1a836ed733f6eeb5b4c47a3aff8d5d4dc642f
ghores <ghazimoradi.amin@gmail.com>
2025-12-26 22:28:57 +0330

    Implementing ApiServices

--- app/src/main/java/com/example/recipeapp/data/network/ApiServices.kt
+++ app/src/main/java/com/example/recipeapp/data/network/ApiServices.kt
@@ -1,4 +1,14 @@
 package com.example.recipeapp.data.network
 
+import com.example.recipeapp.models.register.BodyRegister
+import com.example.recipeapp.models.register.ResponseRegister
+import com.example.recipeapp.utils.Constants.API_KEY
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface ApiServices {
+    @POST("users/connect")
+    suspend fun postRegister(@Query(API_KEY) apikey: String, @Body body: BodyRegister): Response<ResponseRegister>
 }
