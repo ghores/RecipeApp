43d7fd92eda718c115bf1ff87c011d091e718bb7
ghores <ghazimoradi.amin@gmail.com>
2025-12-27 20:37:07 +0330

    Implementation RegisterViewModel

---
+++ app/src/main/java/com/example/recipeapp/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.recipeapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.recipeapp.data.repository.RegisterRepository
+import com.example.recipeapp.models.register.BodyRegister
+import com.example.recipeapp.models.register.ResponseRegister
+import com.example.recipeapp.utils.NetworkRequest
+import com.example.recipeapp.utils.NetworkResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val registerRepository: RegisterRepository) : ViewModel() {
+
+    //Api
+    val registerData = MutableLiveData<NetworkRequest<ResponseRegister>>()
+    fun callRegisterApi(apikey: String, body: BodyRegister) = viewModelScope.launch {
+        registerData.value = NetworkRequest.Loading()
+        val response = registerRepository.postRegister(apikey, body)
+        registerData.value = NetworkResponse(response).generalNetworkResponse()
+    }
+}
