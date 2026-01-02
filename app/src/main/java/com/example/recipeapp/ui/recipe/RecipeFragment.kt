e194b077c39f4c406c34aad0fb0dad8042733df3
ghores <ghazimoradi.amin@gmail.com>
2026-01-02 13:46:28 +0330

    Implementation fragment_recipe.xml ui

--- app/src/main/java/com/example/recipeapp/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipe/RecipeFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentRecipeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecipeFragment : Fragment() {
     //Binding
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentRecipeBinding.inflate(layoutInflater)
         return binding.root
     }
