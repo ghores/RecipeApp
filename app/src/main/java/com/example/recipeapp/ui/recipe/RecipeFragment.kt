2cade2dfad9ab697f8e2e011330d4e957c2d5953
ghores <ghazimoradi.amin@gmail.com>
2026-01-02 18:24:09 +0330

    Implementing the user information display

--- app/src/main/java/com/example/recipeapp/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipe/RecipeFragment.kt
@@ -1,12 +1,18 @@
 package com.example.recipeapp.ui.recipe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.recipeapp.R
 import com.example.recipeapp.databinding.FragmentRecipeBinding
+import com.example.recipeapp.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipeFragment : Fragment() {
@@ -14,9 +20,30 @@ class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
 
+    //Other
+    private val registerViewModel: RegisterViewModel by viewModels()
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentRecipeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Show username
+        lifecycleScope.launch { showUserName() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private suspend fun showUserName() {
+        registerViewModel.readRegisterData.collect {
+            binding.usernameTxt.text = "${getString(R.string.hello)}, ${it.username} ${getEmojiByUnicode()}"
+        }
+    }
+
+    private fun getEmojiByUnicode(): String {
+        return String(Character.toChars(0x1f44b))
     }
 
     override fun onDestroy() {
