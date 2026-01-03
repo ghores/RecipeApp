67a8d6e7b9778073e28f45385bca085d0724789e
ghores <ghazimoradi.amin@gmail.com>
2026-01-03 12:55:22 +0330

    Finishing PopularAdapter

--- app/src/main/java/com/example/recipeapp/adapter/PopularAdapter.kt
+++ app/src/main/java/com/example/recipeapp/adapter/PopularAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.recipeapp.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.example.recipeapp.R
 import com.example.recipeapp.databinding.ItemPopularBinding
 import com.example.recipeapp.models.recipe.ResponseRecipes
 import com.example.recipeapp.utils.BaseDiffUtils
+import com.example.recipeapp.utils.Constants
 import javax.inject.Inject
 
 class PopularAdapter @Inject constructor() : RecyclerView.Adapter<PopularAdapter.ViewHolder>() {
@@ -30,13 +35,33 @@ class PopularAdapter @Inject constructor() : RecyclerView.Adapter<PopularAdapter
     override fun getItemCount(): Int = items.size
 
     inner class ViewHolder() : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(item: ResponseRecipes.Result) {
             binding.apply {
                 //Text
                 popularNameTxt.text = item.title
                 popularPriceTxt.text = "${item.pricePerServing} $"
+                //Image
+                val imageSplit = item.image?.split("-")
+                val imageSize = imageSplit?.get(1)?.replace(Constants.OLD_IMAGE_SIZE, Constants.NEW_IMAGE_SIZE)
+                popularImg.load("${imageSplit?.get(0)}-$imageSize") {
+                    crossfade(true)
+                    crossfade(800)
+                    memoryCachePolicy(CachePolicy.ENABLED)
+                    error(R.drawable.ic_placeholder)
+                }
+                //Click
+                root.setOnClickListener {
+                    onItemClickListener?.let { it(item.id!!) }
+                }
             }
         }
+    }
+
+    private var onItemClickListener: ((Int) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Int) -> Unit) {
+        onItemClickListener = listener
     }
 
     fun setData(data: List<ResponseRecipes.Result>) {
--- app/src/main/java/com/example/recipeapp/utils/Constants.kt
+++ app/src/main/java/com/example/recipeapp/utils/Constants.kt
@@ -19,4 +19,8 @@ object Constants {
     const val REGISTER_USERNAME = "register_username"
     const val REGISTER_USERNAME_INFO = "register_username_info"
     const val REGISTER_HASH = "register_hash"
+
+    //Other
+    const val OLD_IMAGE_SIZE = "312x231.jpg"
+    const val NEW_IMAGE_SIZE = "636x393.jpg"
 }
